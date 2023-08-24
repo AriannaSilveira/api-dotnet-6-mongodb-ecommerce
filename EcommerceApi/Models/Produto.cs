bd1cbc63c28c384ee863c9a409d4e4a96d5a5224
Arianna Silveira <arianna.silveira.s@gmail.com>
2023-08-24 14:20:56 -0300

    feat: modelo de produto e pacote MongoDB.Driver.

---
+++ EcommerceApi/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceApi.Models;
+
+public class Produto
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    public string Nome { get; set; }
+    public float Valor { get; set; }
+    public string Descricao { get; set; }
+
+}
