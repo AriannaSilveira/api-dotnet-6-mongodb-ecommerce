c3ecc16f5bf449c781a3d9b40c7c7c3d4e9fa821
Arianna Silveira <arianna.silveira.s@gmail.com>
2023-08-24 14:45:40 -0300

    feat: conexão com o banco de dados MongoDB.

---
+++ EcommerceApi/Data/ProdutoContext.cs
@@ -0,0 +1,17 @@
+﻿using EcommerceApi.Models;
+using MongoDB.Driver;
+
+namespace EcommerceApi.Data;
+
+public class ProdutoContext
+{
+    private readonly IMongoDatabase _database;
+
+    public ProdutoContext(IConfiguration config)
+    {
+        var client = new MongoClient(config.GetConnectionString("ProdutoConnection"));
+        _database = client.GetDatabase("Ecommerce");
+    }
+
+    public IMongoCollection<Produto> Produtos => _database.GetCollection<Produto>("Produtos");
+}
