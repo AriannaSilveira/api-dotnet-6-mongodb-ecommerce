90ee6c20386b13745333017fe8b8cd466ef2d604
Arianna Silveira <arianna.silveira.s@gmail.com>
2023-08-25 13:23:30 -0300

    feat: filtro de lista de produtos.

--- EcommerceApi/Controllers/ProdutoController.cs
+++ EcommerceApi/Controllers/ProdutoController.cs
@@ -1,8 +1,10 @@
-﻿using AutoMapper;
+﻿using Amazon.Auth.AccessControlPolicy;
+using AutoMapper;
 using EcommerceApi.Data;
 using EcommerceApi.Data.Dtos;
 using EcommerceApi.Models;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace EcommerceApi.Controllers;
@@ -42,10 +44,25 @@ public class ProdutoController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<IActionResult> GetProdutosList()
+    public async Task<IActionResult> GetProdutosList([FromQuery] string? nome = null, [FromQuery] float? precoMaiorQue = null, [FromQuery] float? precoMenorQue = null)
     {
         var constructor = Builders<Produto>.Filter;
         var condition = constructor.Empty;
+
+        if (!string.IsNullOrEmpty(nome))
+        {
+            condition &= constructor.Regex(c => c.Nome, new BsonRegularExpression(nome, "i"));
+        }
+
+        if (precoMaiorQue.HasValue)
+        {
+            condition &= constructor.Gt(c => c.Valor, precoMaiorQue.Value);
+        }
+
+        if (precoMenorQue.HasValue)
+        {
+            condition &= constructor.Lt(c => c.Valor, precoMenorQue.Value);
+        }
 
         var produtos = await _context.Produtos.Find(condition).ToListAsync();
         var produtosDto = _mapper.Map<List<ReadProdutoDto>>(produtos);
